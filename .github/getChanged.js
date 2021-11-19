4dd4bd8f29ae37405b2db1a25d69cd9f0ef15b91
Jakob Harder <12190313+jakobharder@users.noreply.github.com>
2021-11-19 18:36:33 +0100

    build mods with GitHub actions

---
+++ .github/getChanged.js
@@ -0,0 +1,16 @@
+const child_process = require('child_process');
+
+const changed = child_process.execSync('git diff --name-only origin/main HEAD').toString().split('\n').map(e => e.trim());
+
+const hasWorkflowChange = changed.indexOf('.github/') !== -1 || changed.indexOf('package.json') !== -1;
+
+let glob;
+if (hasWorkflowChange) {
+  glob = '**/annomod.json';
+}
+else {
+  const folders = [...new Set(changed.filter(e => e.indexOf('/') !== -1).map(e => e.substring(0, e.indexOf('/'))))];
+  glob = '{' + folders.join(',') + '}/**/annomod.json';
+}
+
+console.log(glob);
