05f2143ab6681c360c1d04f0e710b9bc5843e20b
Jakob Harder <12190313+jakobharder@users.noreply.github.com>
2021-11-21 16:30:19 +0100

    bump to tools v0.5.2
    And disable complete rebuild for workflow changes.

--- .github/getChanged.js
+++ .github/getChanged.js
@@ -2,15 +2,18 @@ const child_process = require('child_process');
 
 const changed = child_process.execSync('git diff --name-only origin/main HEAD').toString().split('\n').map(e => e.trim());
 
-const hasWorkflowChange = changed.indexOf('.github/') !== -1 || changed.indexOf('package.json') !== -1;
+const folders = [...new Set(changed.filter(e => e.indexOf('/') !== -1).map(e => e.substring(0, e.indexOf('/'))))];
+console.log('{' + folders.join(',') + '}/**/annomod.json');
 
-let glob;
-if (hasWorkflowChange) {
-  glob = '**/annomod.json';
-}
-else {
-  const folders = [...new Set(changed.filter(e => e.indexOf('/') !== -1).map(e => e.substring(0, e.indexOf('/'))))];
-  glob = '{' + folders.join(',') + '}/**/annomod.json';
-}
+// const hasWorkflowChange = changed.indexOf('.github/') !== -1 || changed.indexOf('package.json') !== -1;
 
-console.log(glob);
+// let glob;
+// if (hasWorkflowChange) {
+//   glob = '**/annomod.json';
+// }
+// else {
+//   const folders = [...new Set(changed.filter(e => e.indexOf('/') !== -1).map(e => e.substring(0, e.indexOf('/'))))];
+//   glob = '{' + folders.join(',') + '}/**/annomod.json';
+// }
+
+// console.log(glob);
