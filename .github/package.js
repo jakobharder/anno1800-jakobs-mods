4a5fe660c22436e366634cf8725d3894893ce37b
Jakob Harder <12190313+jakobharder@users.noreply.github.com>
2022-02-28 12:19:28 +0100

    fix Train-Stations bugfix folder name

--- .github/package.js
+++ .github/package.js
@@ -9,11 +9,19 @@ const mods = glob.sync("./out/*/").map(e => e.substr(0, e.length - 1));
 mods.forEach(mod => {
   const modinfo = JSON.parse(fs.readFileSync(path.join(mod, 'modinfo.json')));
   versions.push(`${path.basename(mod).replace(/\[.+\] /, '')} ${modinfo.Version}`);
+
+  // allow without [] for bugfixes of legacy mods
+  if (path.basename(mod).startsWith('[] ')) {
+    fs.rmSync(mod.replace(/\[\] /, ''), { recursive: true, force: true });
+    fs.renameSync(mod, mod.replace(/\[\] /, ''));
+  }
 });
 
 fs.writeFileSync('./out/versions.md', versions.join('\n'));
 
-const packages = [... new Set(mods.map(e => {
+// rescan mods
+const modNames = glob.sync("./out/*/").map(e => path.basename(e));
+const packages = [... new Set(modNames.map(e => {
   const dot = e.indexOf(',');
   return (dot !== -1) ? e.substring(0, dot) + ',*' : e;
 }))];
@@ -21,7 +29,7 @@ const packages = [... new Set(mods.map(e => {
 for (let package of packages) {
   child_process.execFileSync('tar', [
     '-c', '-a',
-    '-f', `out/${path.basename(package).replace(/\s/g, '.').replace(',*', '')}.zip`,
-    '-C', 'out/', `${path.basename(package)}`
+    '-f', `out/${package.replace(/\s/g, '.').replace(',*', '')}.zip`,
+    '-C', 'out/', `${package}`
   ])
 }
