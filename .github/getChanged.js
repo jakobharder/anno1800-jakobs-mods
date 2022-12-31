9b8e2a0928b2d6ef9b0399fb865b4bfbd5714c1c
Jakob Harder <12190313+jakobharder@users.noreply.github.com>
2022-12-31 21:23:17 +0100

    pipeline: fix getChanged to work with mods/ folder

--- .github/getChanged.js
+++ .github/getChanged.js
@@ -1,13 +1,23 @@
 const child_process = require('child_process');
+const fs = require('fs');
+const path = require('path');
 
 const changed = child_process.execSync('git diff --name-only origin/main HEAD').toString().split('\n').map(e => e.trim());
 
-const folders = [...new Set(changed.filter(e => e.indexOf('/') !== -1).map(e => e.substring(0, e.indexOf('/'))))];
+function getModName(changedPath) {
+  split = changedPath.split("/");
+  if (split.length > 1) {
+    return path.join(split[0], split[1]);
+  }
+  return undefined;
+}
+
+const folders = [...new Set(changed.map(e => getModName(e)).filter(e => e !== undefined && fs.existsSync(path.join(e, "annomod.json"))))];
 if (folders.length === 0 || !folders[0]) {
   return;
 }
 else if (folders.length === 1) {
-  console.log(`${folders[0]}/**/annomod.json`);
+  console.log(`${folders[0]}/annomod.json`);
   return;
 }
-console.log(`{${folders.join(',')}}/**/annomod.json`);
+console.log(`{${folders.join(',')}}/annomod.json`);
