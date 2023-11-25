b534425b31d6d878485351886ed55fafac2b5b11
Jakob Harder <12190313+jakobharder@users.noreply.github.com>
2023-11-25 13:31:06 +0100

    fix zip packaging

--- .github/package.js
+++ .github/package.js
@@ -7,7 +7,8 @@ function readMeta(filePath) {
   const modinfo = JSON.parse(fs.readFileSync(filePath));
   return {
     name: modinfo.ModName.English.replace(' (Jakob)', '').replace(/ /g, '-').replace(',', '').replace('\'', ''),
-    folder: `[${modinfo.Category.English}] ${modinfo.ModName.English}`,
+    // folder: `[${modinfo.Category.English}] ${modinfo.ModName.English}`,
+    path: path.dirname(filePath),
     version: modinfo.Version,
     id: modinfo.ModID,
     dependencies: modinfo.ModDependencies
@@ -45,7 +46,7 @@ const idsToPackage = [
   "jakob_compact_menus"
 ];
 
-const modsToPackage = glob.sync("./mods/**/modinfo.json")
+const modsToPackage = glob.sync("./out/*/modinfo.json")
   .map(e => readMeta(e))
   // .filter(e => fs.existsSync(path.join('out', e.folder)));
   .filter(e => idsToPackage.indexOf(e.id) >= 0);
@@ -55,12 +56,7 @@ const modsToInclude = glob.sync("./{mods,shared}/**/modinfo.json")
 for (mod of modsToPackage) {
   // const sharedMods = modsToInclude.filter(e => mod.ModDependencies?.indexOf(e.id) >= 0);
   console.log(`Package ${mod.name}-${mod.version}.zip`);
-  // for (shared of sharedMods) {
-  //   copyFolderSync(path.join('out', shared.folder), path.join('out', mod.folder, shared.folder));
-  // }
-  child_process.execFileSync('tar', [
-      '-ca',
-      '-f', path.join(process.cwd(), `out/${mod.name}-${mod.version}.zip`),
-      '-C', path.join(process.cwd(), 'out/'), `"${mod.folder}"`
-    ]);
+
+  child_process.execSync(
+    `tar -ca -f ${path.join(process.cwd(), `out/${mod.name}-${mod.version}.zip`)} -C ${path.join(process.cwd(), path.dirname(mod.path))} "${path.basename(mod.path)}"`);
 }
